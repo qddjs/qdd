8653055270f6cd160aeeac1f2bedece738f243f3
Bryan English <bryan@bryanenglish.com>
2018-03-17 23:45:12 -0700

    switch from prettier to semistandard

--- bench.js
+++ bench.js
@@ -1,49 +1,49 @@
-const path = require("path");
-const util = require("util");
-const child_process = require("child_process");
-const exec = util.promisify(child_process.exec);
+const path = require('path');
+const util = require('util');
+const { exec: execCb } = require('child_process');
+const exec = util.promisify(execCb);
 
 const freshNpmTimes = [];
 const freshQddTimes = [];
 const primedNpmTimes = [];
 const primedQddTimes = [];
 
-async function freshNpm() {
+async function freshNpm () {
   await exec(`npm cache clean --force`);
   await exec(`rm -rf testapp/node_modules`);
   const start = process.hrtime();
-  await exec(`npm ci`, { cwd: path.join(__dirname, "testapp") });
+  await exec(`npm ci`, { cwd: path.join(__dirname, 'testapp') });
   const elapsed = process.hrtime(start);
   const time = elapsed[0] + elapsed[1] / 1e9;
   freshNpmTimes.push(time);
   console.log(' fresh cache npm ci:', time, 'seconds');
 }
 
-async function freshQdd() {
+async function freshQdd () {
   await exec(`rm -rf ~/.cache/qdd`);
   await exec(`rm -rf testapp/node_modules`);
   const start = process.hrtime();
-  await exec("node ../index.js", { cwd: path.join(__dirname, "testapp") });
+  await exec('node ../index.js', { cwd: path.join(__dirname, 'testapp') });
   const elapsed = process.hrtime(start);
   const time = elapsed[0] + elapsed[1] / 1e9;
   freshQddTimes.push(time);
   console.log('    fresh cache qdd:', time, 'seconds');
 }
 
-async function primedNpm() {
+async function primedNpm () {
   await exec(`rm -rf testapp/node_modules`);
   const start = process.hrtime();
-  await exec(`npm ci`, { cwd: path.join(__dirname, "testapp") });
+  await exec(`npm ci`, { cwd: path.join(__dirname, 'testapp') });
   const elapsed = process.hrtime(start);
   const time = elapsed[0] + elapsed[1] / 1e9;
   primedNpmTimes.push(time);
   console.log('primed cache npm ci:', time, 'seconds');
 }
 
-async function primedQdd() {
+async function primedQdd () {
   await exec(`rm -rf testapp/node_modules`);
   const start = process.hrtime();
-  await exec("node ../index.js", { cwd: path.join(__dirname, "testapp") });
+  await exec('node ../index.js', { cwd: path.join(__dirname, 'testapp') });
   const elapsed = process.hrtime(start);
   const time = elapsed[0] + elapsed[1] / 1e9;
   primedQddTimes.push(time);
@@ -52,7 +52,7 @@ async function primedQdd() {
 
 const TIMES = 10;
 
-function average(nums) {
+function average (nums) {
   let total = 0;
   for (const i of nums) {
     total += i;
@@ -69,8 +69,8 @@ function average(nums) {
     await primedNpm();
     await primedQdd();
   }
-  console.log(" fresh cache npm ci (avg):", average(freshNpmTimes), 'seconds');
-  console.log("    fresh cache qdd (avg):", average(freshQddTimes), 'seconds');
-  console.log("primed cache npm ci (avg):", average(primedNpmTimes), 'seconds');
-  console.log("   primed cache qdd (avg):", average(primedQddTimes), 'seconds');
+  console.log(' fresh cache npm ci (avg):', average(freshNpmTimes), 'seconds');
+  console.log('    fresh cache qdd (avg):', average(freshQddTimes), 'seconds');
+  console.log('primed cache npm ci (avg):', average(primedNpmTimes), 'seconds');
+  console.log('   primed cache qdd (avg):', average(primedQddTimes), 'seconds');
 })();
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-const config = require("./lib/config.js");
-const cp = require("./lib/cp.js");
-const download = require("./lib/download.js");
+const config = require('./lib/config.js');
+const cp = require('./lib/cp.js');
+const download = require('./lib/download.js');
 const mkdirp = require('./lib/mkdirp.js');
-const isdir = require("./lib/isdir.js");
+const isdir = require('./lib/isdir.js');
 
-const fs = require("fs");
-const util = require("util");
+const fs = require('fs');
+const util = require('util');
 
 const isProd =
-  "QDD_PROD" in process.env ||
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "prod";
+  'QDD_PROD' in process.env ||
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'prod';
 
 if (fs.existsSync(`${process.cwd()}/node_modules`)) {
-  console.error("Please delete your node_modules directory before installing.");
+  console.error('Please delete your node_modules directory before installing.');
   process.exit(1);
 }
 
 const tree = require(`${process.cwd()}/package-lock.json`);
 
 const todos = [];
-function install(mod, dir) {
+function install (mod, dir) {
   const deps = mod.dependencies;
   if (!deps) return;
 
@@ -38,13 +38,13 @@ function install(mod, dir) {
 
     const { integrity, resolved: url } = entry;
     if (!integrity || !url) {
-      console.error("invalid entry found in package-lock.json");
+      console.error('invalid entry found in package-lock.json');
       console.error(util.inspect({ [name]: entry }));
-      console.error("fix this, delete node_modules, and try again");
+      console.error('fix this, delete node_modules, and try again');
       process.exit(1);
     }
     const destDir = `${dir}/node_modules/${name}`;
-    const cacheDir = config.cacheDir + '/' + integrity
+    const cacheDir = config.cacheDir + '/' + integrity;
     todos.push(installOne.bind(null, name, integrity, url, destDir, cacheDir));
     if (entry.dependencies) {
       install(entry, destDir);
@@ -52,7 +52,7 @@ function install(mod, dir) {
   }
 }
 
-function installOne(name, integrity, url, destDir, cacheDir, cb) {
+function installOne (name, integrity, url, destDir, cacheDir, cb) {
   isdir(cacheDir, (err, isDir) => {
     if (err || !isDir) {
       return download(cacheDir, url, integrity, destDir, cb);
@@ -67,7 +67,6 @@ mkdirp(config.cacheDir, (err) => {
   if (err) {
     throw err;
   }
-  const toRun = todos.length;
   for (const fn of todos) {
     fn(err => {
       if (err) {
