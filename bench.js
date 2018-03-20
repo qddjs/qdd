bec9de60c3aff2a973f3f4abc6e28186e871e4ea
Bryan English <bryan@bryanenglish.com>
2018-03-20 05:21:34 -0700

    add yarn to bench
    Also pin npm to npm@next.

--- bench.js
+++ bench.js
@@ -4,8 +4,10 @@ const { exec: execCb } = require('child_process');
 const exec = util.promisify(execCb);
 
 const freshNpmTimes = [];
+const freshYarnTimes = [];
 const freshQddTimes = [];
 const primedNpmTimes = [];
+const primedYarnTimes = [];
 const primedQddTimes = [];
 
 async function freshNpm () {
@@ -17,6 +19,17 @@ async function freshNpm () {
   const time = elapsed[0] + elapsed[1] / 1e9;
   freshNpmTimes.push(time);
   console.log(' fresh cache npm ci:', time, 'seconds');
+}
+
+async function freshYarn () {
+  await exec(`rm -rf testapp/.yarn-cache`);
+  await exec(`rm -rf testapp/node_modules`);
+  const start = process.hrtime();
+  await exec(`yarn --ignore-scripts --cache-folder .yarn-cache`, { cwd: path.join(__dirname, 'testapp') });
+  const elapsed = process.hrtime(start);
+  const time = elapsed[0] + elapsed[1] / 1e9;
+  freshYarnTimes.push(time);
+  console.log('   fresh cache yarn:', time, 'seconds');
 }
 
 async function freshQdd () {
@@ -39,6 +52,16 @@ async function primedNpm () {
   primedNpmTimes.push(time);
   await exec(`rm -rf testapp/.npm-cache`);
   console.log('primed cache npm ci:', time, 'seconds');
+}
+
+async function primedYarn () {
+  await exec(`rm -rf testapp/node_modules`);
+  const start = process.hrtime();
+  await exec(`yarn --ignore-scripts --cache-folder .yarn-cache`, { cwd: path.join(__dirname, 'testapp') });
+  const elapsed = process.hrtime(start);
+  const time = elapsed[0] + elapsed[1] / 1e9;
+  primedYarnTimes.push(time);
+  console.log('  primed cache yarn:', time, 'seconds');
 }
 
 async function primedQdd () {
@@ -64,14 +87,18 @@ function average (nums) {
 (async () => {
   for (let i = 0; i < TIMES; i++) {
     await freshNpm();
+    await freshYarn();
     await freshQdd();
   }
   for (let i = 0; i < TIMES; i++) {
     await primedNpm();
+    await primedYarn();
     await primedQdd();
   }
   console.log(' fresh cache npm ci (avg):', average(freshNpmTimes), 'seconds');
+  console.log('   fresh cache yarn (avg):', average(freshYarnTimes), 'seconds');
   console.log('    fresh cache qdd (avg):', average(freshQddTimes), 'seconds');
   console.log('primed cache npm ci (avg):', average(primedNpmTimes), 'seconds');
+  console.log('  primed cache yarn (avg):', average(primedYarnTimes), 'seconds');
   console.log('   primed cache qdd (avg):', average(primedQddTimes), 'seconds');
 })();
