2c8260aa7c1bb448b9aa47dd4c3e1e47b729b3c5
Bryan English <bryan@bryanenglish.com>
2018-03-30 23:33:18 -0700

    testing node 6

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const isdir = require('./lib/isdir.js');
 const fs = require('fs');
 const util = require('util');
 
+if (!Object.entries) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+  Object.entries = function(obj){
+    const ownProps = Object.keys(obj);
+    let i = ownProps.length;
+    const resArray = new Array(i); // preallocate the Array
+    while (i--) {
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    }
+    return resArray;
+  };
+}
 const isProd =
   'QDD_PROD' in process.env ||
   process.env.NODE_ENV === 'production' ||
--- test.js
+++ test.js
@@ -1,42 +1,41 @@
 'use strict';
 
 const assert = require('assert');
-const { exec: execCb } = require('child_process');
+const { execSync: exec } = require('child_process');
 const util = require('util');
 const path = require('path');
-const exec = util.promisify(execCb);
 
 const test = require('pitesti')({ timeout: 60000 });
 
-test`fresh cache trees are equal`(async () => {
-  await exec(`rm -rf testapp/node_modules`);
-  await exec(`npm ci`, { cwd: path.join(__dirname, 'testapp') });
-  const npmTree = (await exec(`tree -s ${__dirname}/testapp/node_modules`, {
+test`fresh cache trees are equal`(() => {
+  exec(`rm -rf testapp/node_modules`);
+  exec(`npm ci`, { cwd: path.join(__dirname, 'testapp') });
+  const npmTree = exec(`tree -s ${__dirname}/testapp/node_modules`, {
     maxBuffer: 100 * 1024 * 1024
-  })).stdout.replace(/\[ +\d+\] {2}package.json/g, 'package.json');
+  }).toString().replace(/\[ +\d+\] {2}package.json/g, 'package.json');
   // ^ package.jsons have different sizes
-  await exec(`rm -rf ~/.cache/qdd`);
-  await exec(`rm -rf testapp/node_modules`);
-  await exec('node --no-warnings ../index.js', { cwd: path.join(__dirname, 'testapp') });
-  const qddTree = (await exec(`tree -s ${__dirname}/testapp/node_modules`, {
+  exec(`rm -rf ~/.cache/qdd`);
+  exec(`rm -rf testapp/node_modules`);
+  exec('node --no-warnings ../index.js', { cwd: path.join(__dirname, 'testapp') });
+  const qddTree = exec(`tree -s ${__dirname}/testapp/node_modules`, {
     maxBuffer: 100 * 1024 * 1024
-  })).stdout.replace(/\[ +\d+\] {2}package.json/g, 'package.json');
+  }).toString().replace(/\[ +\d+\] {2}package.json/g, 'package.json');
   // ^ package.jsons have different sizes
   assert.strictEqual(npmTree, qddTree);
 });
 
-test`primed cache trees are equal`(async () => {
-  await exec(`rm -rf testapp/node_modules`);
-  await exec(`npm ci`, { cwd: path.join(__dirname, 'testapp') });
-  const npmTree = (await exec(`tree -s ${__dirname}/testapp/node_modules`, {
+test`primed cache trees are equal`(() => {
+  exec(`rm -rf testapp/node_modules`);
+  exec(`npm ci`, { cwd: path.join(__dirname, 'testapp') });
+  const npmTree = exec(`tree -s ${__dirname}/testapp/node_modules`, {
     maxBuffer: 100 * 1024 * 1024
-  })).stdout.replace(/\[ +\d+\] {2}package.json/g, 'package.json');
+  }).toString().replace(/\[ +\d+\] {2}package.json/g, 'package.json');
   // ^ package.jsons have different sizes
-  await exec(`rm -rf testapp/node_modules`);
-  await exec('node --no-warnings ../index.js', { cwd: path.join(__dirname, 'testapp') });
-  const qddTree = (await exec(`tree -s ${__dirname}/testapp/node_modules`, {
+  exec(`rm -rf testapp/node_modules`);
+  exec('node --no-warnings ../index.js', { cwd: path.join(__dirname, 'testapp') });
+  const qddTree = exec(`tree -s ${__dirname}/testapp/node_modules`, {
     maxBuffer: 100 * 1024 * 1024
-  })).stdout.replace(/\[ +\d+\] {2}package.json/g, 'package.json');
+  }).toString().replace(/\[ +\d+\] {2}package.json/g, 'package.json');
   // ^ package.jsons have different sizes
   assert.strictEqual(npmTree, qddTree);
 });
